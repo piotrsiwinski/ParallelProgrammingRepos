ddd3180b0984a22a4383943c3d21f944fe89b6db
Piotr Siwinski <siwinski.piotr@outlook.com>
2016-06-06 21:51:49 +0200

    Zadanie 3

---
+++ Zadanie1/Zadanie2/Zad1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    public class ArrayAsync
+    {
+        private static readonly object _locker = new object();
+        private readonly double[] _array;
+        private readonly int _threadsCount;
+        private readonly Thread[] _threads;
+        private readonly Random _random;
+        public double Sum { get; set; }
+
+        public ArrayAsync(int arraySize, int threadsCount = 4)
+        {
+            _random = new Random();
+            _array = new double[arraySize];
+            _threadsCount = threadsCount;
+            _threads = new Thread[threadsCount];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                _array[i] = _random.NextDouble();
+            }
+        }
+
+        public double SumArrayAsync()
+        {
+            for (int i = 0; i < _threadsCount; i++)
+            {
+                var temp = i;
+                _threads[i] = new Thread(() =>
+                {
+                    SumArrayUsingThread(temp);
+                });
+                _threads[i].Start();
+                _threads[i].Join();
+            }
+
+            return Sum;
+        }
+
+        private void SumArrayUsingThread(int index)
+        {
+            double result = 0;
+            for (int i = index; i < _array.Length; i += _threadsCount)
+            {
+                result += _array[i];
+            }
+
+            lock (_locker)
+            {
+                Sum += result;
+            }
+        }
+
+        public double SumArray()
+        {
+            return _array.Sum();
+        }
+    }
+
+    class Zad1
+    {
+        static void Main(string[] args)
+        {
+            ArrayAsync arrayAsync = new ArrayAsync(100);
+            Console.WriteLine($"Async sum {arrayAsync.SumArrayAsync()}");
+            Console.WriteLine($"Sync sum {arrayAsync.SumArray()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadanie1/Zadanie3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Security;
+namespace Zadanie3
+{
+    public class ProducerConsumer
+    {
+        private static AutoResetEvent _producerAutoResetEvent;
+        private static AutoResetEvent _consumerAutoResetEvent;
+        
+        private Thread _producerThread;
+        private Thread _consumerThread;
+        private List<string> bufferList;
+        private int _bufferSize;
+        private string _password;
+        private bool _passwordFound;
+
+        public ProducerConsumer(int bufferSize, string password)
+        {
+            _producerAutoResetEvent = new AutoResetEvent(false);
+            _consumerAutoResetEvent = new AutoResetEvent(false);
+            _bufferSize = bufferSize;
+            _password = password;
+            _passwordFound = false;
+            bufferList = new List<string>(_bufferSize);
+
+            _producerThread = new Thread(Produce);
+            _consumerThread = new Thread(Consume);
+
+            _producerThread.Start();
+            _consumerThread.Start();
+            _producerThread.Join();
+
+            
+            _consumerThread.Join();
+
+
+        }
+
+        private void Produce()
+        {
+            while (!_passwordFound)
+            {
+                Console.WriteLine("Producing passwords");
+                //Thread.Sleep(1000);
+                while (bufferList.Count < _bufferSize)
+                {
+                    bufferList.Add(Membership.GeneratePassword(_password.Length, 0));
+                }
+                Console.WriteLine("Produced passwords");
+                _consumerAutoResetEvent.Set();
+                _producerAutoResetEvent.WaitOne();
+            }
+           
+        }
+
+        private void Consume()
+        {
+            while (!_passwordFound)
+            {
+                Console.WriteLine("Consumer waiting for producer...");
+                _consumerAutoResetEvent.WaitOne();
+                Console.WriteLine("Receiving from buffer");
+                //Thread.Sleep(1000);
+                foreach (var element in bufferList)
+                {
+                    if (element == _password)
+                    {
+                        _passwordFound = true;
+                        Console.WriteLine($"Password found. Password is: {element}");
+                        break;
+                    }
+                    
+                }
+                bufferList.Clear();
+                _producerAutoResetEvent.Set();
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProducerConsumer producerConsumer = new ProducerConsumer(10, "a");
+
+
+            Console.ReadLine();
+        }
+    }
+}
