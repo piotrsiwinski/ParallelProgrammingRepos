476b9bd9fd199c7fce4c860bb75c2d5c3e58ff6e
Piotr Siwinski <siwinski.piotr@outlook.com>
2016-06-09 11:56:22 +0200

    zad2

--- ParallelProgramming/Zadanie1.2/Zad2.cs
+++ ParallelProgramming/Zadanie1.2/Zad2.cs
@@ -1,7 +1,51 @@
-﻿namespace Zadanie1
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApplication1
 {
-    public class Zad2
+
+    class Program
     {
-        
+       
+        static int CreatedThreads = 0;
+        static object lockMethod = new object();
+        static void IncrementNumberOfThreads()
+        {
+            lock (lockMethod)
+            {
+                ++CreatedThreads;
+            }
+        }
+
+        public static void CreateThread()
+        {
+            try
+            {
+                IncrementNumberOfThreads();
+
+                Console.WriteLine($"Thread nr: {CreatedThreads} and id {Thread.CurrentThread.ManagedThreadId}");
+                Thread thread = new Thread(CreateThread);
+                thread.Start();
+                thread.Join();
+                Console.WriteLine($"Thread numer {Thread.CurrentThread.ManagedThreadId} is exiting");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Max number of threads: {CreatedThreads}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread thr = new Thread(CreateThread);
+                thr.Start();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exiting");
+            }
+        }
     }
 }
